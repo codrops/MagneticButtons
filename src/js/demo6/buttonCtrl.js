fcfc3eb60d7e657745e61d95fc840c124790b842
botelho <botelho@users.noreply.github.com>
2021-02-24 09:47:48 +0000

    Update buttonCtrl.js
    the scroll value was not being considered for the mouse position calculations

--- src/js/demo6/buttonCtrl.js
+++ src/js/demo6/buttonCtrl.js
@@ -50,7 +50,7 @@ export default class ButtonCtrl extends EventEmitter {
     }
     render() {
         // calculate the distance from the mouse to the center of the button
-        const distanceMouseButton = distance(mousepos.x, mousepos.y, this.rect.left + this.rect.width/2, this.rect.top + this.rect.height/2);
+        const distanceMouseButton = distance(mousepos.x+window.scrollX, mousepos.y+window.scrollY, this.rect.left + this.rect.width/2, this.rect.top + this.rect.height/2);
         // new values for the translations
         let x = 0;
         let y = 0;
@@ -61,10 +61,10 @@ export default class ButtonCtrl extends EventEmitter {
             if ( !this.state.hover ) {
                 this.enter();
             }
-            x = (mousepos.x - (this.rect.left + this.rect.width/2))*.2;
-            y = (mousepos.y - (this.rect.top + this.rect.height/2))*.2;
-            rx = (mousepos.y - (this.rect.top + this.rect.height/2))*.07;
-            ry = (mousepos.x - (this.rect.left + this.rect.width/2))*.09;
+            x = (mousepos.x + window.scrollX - (this.rect.left + this.rect.width/2))*.2;
+            y = (mousepos.y + window.scrollY - (this.rect.top + this.rect.height/2))*.2;
+            rx = (mousepos.y + window.scrollY - (this.rect.top + this.rect.height/2))*.07;
+            ry = (mousepos.x + window.scrollX - (this.rect.left + this.rect.width/2))*.09;
         }
         else if ( this.state.hover ) {
             this.leave();
