7138d82cf7b9498498e4d3c77a55ff56bd7f9dab
botelho <botelho@users.noreply.github.com>
2021-02-24 09:40:12 +0000

    Update utils.js
    the scroll value was not being considered for the mouse position calculations

--- src/js/utils.js
+++ src/js/utils.js
@@ -9,20 +9,11 @@ const calcWinsize = () => {
 };
 
 // Gets the mouse position
-const getMousePos = (e) => {
-    let posx = 0;
-    let posy = 0;
-    if (!e) e = window.event;
-    if (e.pageX || e.pageY) {
-        posx = e.pageX;
-        posy = e.pageY;
-    }
-    else if (e.clientX || e.clientY)    {
-        posx = e.clientX + body.scrollLeft + document.documentElement.scrollLeft;
-        posy = e.clientY + body.scrollTop + document.documentElement.scrollTop;
-    }
-    
-    return { x : posx, y : posy }
+const getMousePos = e => {
+    return { 
+        x : e.clientX, 
+        y : e.clientY 
+    };
 };
 
 const distance = (x1,y1,x2,y2) => {
